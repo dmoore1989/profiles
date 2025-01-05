e70aea7ce988a62ed7ed78ae8289f5fe19e968e8
Douglas Moore <dmoore@justworks.com>
2025-01-04 21:27:52 -0500

    Additional packages pulled into modules

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 -- DOUG'S SUPER COOL NVIM CONFIG!!!!
+-- A Fork of the kickstart nvim
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -7,6 +8,7 @@ vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
+
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
