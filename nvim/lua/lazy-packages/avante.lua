f3241c58634e8ab60cea1882500412e1bf461463
Douglas Moore <dmoore@justworks.com>
2025-03-30 16:50:43 -0400

    Add hot key and fix config bug

--- nvim/lua/lazy-packages/avante.lua
+++ nvim/lua/lazy-packages/avante.lua
@@ -11,9 +11,14 @@ return {
       model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
       temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
       --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
     },
+    mappings = {
+      sidebar = {
+        close = 'q',
+      },
+    },
+    debug = true,
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
